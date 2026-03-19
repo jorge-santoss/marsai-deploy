834af103f8a1074c08b0de7c262abcb6ae804607
Jorge Santos <jorge.santos@laplateforme.io>
2026-03-19 12:20:54 +0100

    Add ngrok-skip-browser-warning header to Axios instance configuration

--- src/api/config.js
+++ src/api/config.js
@@ -8,6 +8,9 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   timeout: 10000,
+  headers: {
+    'ngrok-skip-browser-warning': 'true'
+  },
 });
 
 /**
